e89b5acd6b99ce137812b1f929e5952201f596ea
Pavel Chunyayev <pchunyayev@gmail.com>
2015-03-16 19:42:01 +0100

    Yahtzee implementation.

---
+++ week 4/yahtzee.py
@@ -0,0 +1,120 @@
+"""
+Planner for Yahtzee
+Simplifications:  only allow discard and roll, only score against upper level
+"""
+
+# Used to increase the timeout, if necessary
+import codeskulptor
+codeskulptor.set_timeout(20)
+
+def gen_all_sequences(outcomes, length):
+    """
+    Iterative function that enumerates the set of all sequences of
+    outcomes of given length.
+    """
+    
+    answer_set = set([()])
+    for dummy_idx in range(length):
+        temp_set = set()
+        for partial_sequence in answer_set:
+            for item in outcomes:
+                new_sequence = list(partial_sequence)
+                new_sequence.append(item)
+                temp_set.add(tuple(new_sequence))
+        answer_set = temp_set
+    return answer_set
+
+
+def score(hand):
+    """
+    Compute the maximal score for a Yahtzee hand according to the
+    upper section of the Yahtzee score card.
+
+    hand: full yahtzee hand
+
+    Returns an integer score 
+    """
+    scores = {}
+    for die in hand:
+        if die in scores:
+            scores[die] += die
+        else:
+            scores[die] = die
+    return max(scores.values())
+
+
+def expected_value(held_dice, num_die_sides, num_free_dice):
+    """
+    Compute the expected value based on held_dice given that there
+    are num_free_dice to be rolled, each with num_die_sides.
+
+    held_dice: dice that you will hold
+    num_die_sides: number of sides on each die
+    num_free_dice: number of dice to be rolled
+
+    Returns a floating point expected value
+    """
+    rolls = gen_all_sequences(range(1, num_die_sides+1), num_free_dice)
+    hands_sum = 0
+    for roll in rolls:
+        hand_after_roll = held_dice + roll
+        hands_sum += score(hand_after_roll)
+    return float(hands_sum)/len(rolls)
+
+def gen_all_holds(hand):
+    """
+    Generate all possible choices of dice from hand to hold.
+
+    hand: full yahtzee hand
+
+    Returns a set of tuples, where each tuple is dice to hold
+    """
+    all_holds = set([()])
+    for item in hand:
+        for cur_hold in all_holds.copy():
+            copy_cur_hold = list(cur_hold)
+            copy_cur_hold.append(item)
+            all_holds.add(tuple(copy_cur_hold))  
+    return all_holds
+
+
+
+def strategy(hand, num_die_sides):
+    """
+    Compute the hold that maximizes the expected value when the
+    discarded dice are rolled.
+
+    hand: full yahtzee hand
+    num_die_sides: number of sides on each die
+
+    Returns a tuple where the first element is the expected score and
+    the second element is a tuple of the dice to hold
+    """
+    all_holds = gen_all_holds(hand)
+    max_score = 0.0
+    best_hold = ()
+    for hold in all_holds:
+        value = expected_value(hold, num_die_sides, len(hand) - len(hold))
+        if value > max_score:
+            max_score = value
+            best_hold = hold
+    return (max_score, best_hold)
+
+
+def run_example():
+    """
+    Compute the dice to hold and expected score for an example hand
+    """
+    num_die_sides = 6
+    hand = (1, 1, 1, 5, 6)
+    #num_die_sides = 2
+    #hand = (1, 3)
+    hand_score, hold = strategy(hand, num_die_sides)
+    print "Best strategy for hand", hand, "is to hold", hold, "with expected score", hand_score
+    
+    
+run_example()
+
+
+#import poc_holds_testsuite
+#poc_holds_testsuite.run_suite(gen_all_holds)
